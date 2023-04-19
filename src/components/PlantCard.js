03914b12f5fc2110d7b256ecd0839308bbc69287
Bruce <bruce.hillsworth@gmail.com>
2023-04-19 12:02:33 -0600

    Added state to PokemonPage

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,17 +1,20 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({ plants }) {
+  // const [image, name, price] = plants;
+
   return (
-    <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
-    </li>
+    <h1>Hi</h1>
+    // <li className="card">
+    //   <img src={image} alt={name} />
+    //   <h4>{name}</h4>
+    //   <p>Price: {price}</p>
+    //   {true ? (
+    //     <button className="primary">In Stock</button>
+    //   ) : (
+    //     <button>Out of Stock</button>
+    //   )}
+    // </li>
   );
 }
 
